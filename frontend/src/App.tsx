e4ee719d06cfc3c6320fb6b865abfbb877f4b804
katogeweed <rktaro072997@gmail.com>
2026-06-28 11:18:43 +0900

    スコアを表示するようにした

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,21 +8,37 @@ function App(){
   // 今何問目を解いているかを記録する
   const [currentQuestionIndex,setCurrentQuestionIndex]=useState(0)
 
+  const [score,setScore] = useState(0)
+  const [isFinished,setIsFinished] = useState(false)
+
   const currentQuiz = quizData[currentQuestionIndex]
 
   const handleAnswerClick = (selectedIndex: number) => {
-    if (selectedIndex === currentQuiz.correct) 
-      alert("正解")
-    else
+    if (selectedIndex === currentQuiz.correct) {
+      alert("正解"),
+      setScore(score + 1)}
+    else{
       alert("不正解")
+    }
+  
 
     const nextIndex = currentQuestionIndex + 1
     console.log(nextIndex)
     if (nextIndex < quizData.length){
       setCurrentQuestionIndex(nextIndex);
     }else{
-      alert("全問終了")
+      setIsFinished(true)
     }
+  }
+
+  if(isFinished){
+    return(
+      <div>
+        <h1>クイズ終了！</h1>
+        <p>{quizData.length}問中{score}正解！</p>
+      </div>
+    )
+
   }
 
 
