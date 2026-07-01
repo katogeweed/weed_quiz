5f872bedfe30c323388938633acaabce0c2dad4a
katogeweed <rktaro072997@gmail.com>
2026-07-01 21:43:02 +0900

    残り

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,40 @@
 import { useEffect, useState } from 'react'
-import { quizData } from './questions'
 import './App.css'
 
-
+type Quiz = {
+  id: number;
+  context: string;
+  options: string[];
+  answer: string;
+}
 
 function App(){
   // 今何問目を解いているかを記録する
   const [currentQuestionIndex,setCurrentQuestionIndex]=useState(0)
+
   const [score,setScore] = useState(0)
   const [isFinished,setIsFinished] = useState(false)
 
-  const [backendMessage, setBackendMessage] = useState("")
+  const [quizzes, setQuizzes] = useState<Quiz[]>([])
   useEffect(() => {
     fetch("http://localhost:8080/")
       .then(response => response.json())            
       //.then(...)（〜が終わったら、次に）
       // 通信は一瞬ではなく「待ち時間」が発生します。そのため、「データが届くのを待って、届いたら次にこの処理をやってね」と予約をするのが .then
       .then(data => {
-        setBackendMessage(data.message)
+        setQuizzes(data)
       })
       .catch(error => console.error("通信エラー:",error))//thenがうまく行かなかった時にブロックするのがcatch
   }, [])//最後のからの配列は超重要で画面が最初に表示された時の『1回だけ』実行してね」というおまじない
 
-  const currentQuiz = quizData[currentQuestionIndex]
+  if (quizzes.length === 0){
+    return <div>クイズを読み込み中...</div>
+  }
 
-  const handleAnswerClick = (selectedIndex: number) => {
-    if (selectedIndex === currentQuiz.correct) {
+  const currentQuiz = quizzes[currentQuestionIndex]
+
+  const handleAnswerClick = (selectedOptions: string) => {
+    if (selectedOptions === currentQuiz.answer) {
       alert("正解"),
       setScore(score + 1)}
     else{
@@ -35,7 +44,7 @@ function App(){
 
     const nextIndex = currentQuestionIndex + 1
     console.log(nextIndex)
-    if (nextIndex < quizData.length){
+    if (nextIndex < quizzes.length){
       setCurrentQuestionIndex(nextIndex);
     }else{
       setIsFinished(true)
@@ -46,7 +55,7 @@ function App(){
     return(
       <div>
         <h1>クイズ終了！</h1>
-        <p>{quizData.length}問中{score}正解！</p>
+        <p>{quizzes.length}問中{score}正解！</p>
       </div>
     )
 
@@ -57,18 +66,14 @@ function App(){
     <div>
       <h1>雑草クイズアプリ</h1>
 
-      <p style={{ color: "blue",fontWeight: "bold"}}>
-        バックエンド通信{backendMessage}
-      </p>
-
       <p>
         {currentQuiz.context}
       </p>
       <div>
-          {currentQuiz.choices.map((choice,index) =>(
+          {currentQuiz.options.map((choice,index) =>(
             <button key={index} 
             //クリックされたら handleAnswerClick に index を渡して実行するという無名関数をその場で作って、ボタンに渡している
-              onClick={()=>handleAnswerClick(index)}
+              onClick={()=>handleAnswerClick(choice)}
             >{choice}</button>
           ))}
       </div>
